a334efc3dc4cebe21756ff15ebb5bd74dace7c4d
GarryLaly <garrylaly@gmail.com>
2022-12-07 09:28:05 +0700

    Setup font Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      lato: ["Lato", "Arial", "sans-serif"],
+    },
+    extend: {
+      maxWidth: {
+        "500px": "500px",
+      },
+    },
   },
   plugins: [],
-}
+};
